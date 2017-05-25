ae4fbc60402872e13e7b6630f1051b656eb41dfa
John Halmaghi <john.g.halmaghi@gmail.com>
2017-05-24 17:05:16 -0700

    Made it more responsive to resizing

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function mouseTrapBindings() {
-    Mousetrap.bind('g', function() { window.open("https://github.com", "_self"); });
-    Mousetrap.bind("?", function() { console.log('show shortcuts!'); });
-    Mousetrap.bind('esc', function() { console.log('escape'); }, 'keyup');
-
-};
